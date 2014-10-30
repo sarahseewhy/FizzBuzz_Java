5be36017c61e047f1dc607e04712f098197e6b65
Sarah <y.sarahc@gmail.com>
2014-10-30 11:22:28 +0000

    Refactors tests

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -1,19 +1,20 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FizzBuzzTest {
 
+    FizzBuzz game = new FizzBuzz();
+
     @Test
     public void divisibleByThree() {
-        FizzBuzz game = new FizzBuzz();
         boolean result = game.divisibleByThree(3);
         assertEquals(true, result);
     }
 
     @Test
     public void notDivisibleByThree() {
-        FizzBuzz game = new FizzBuzz();
         boolean result = game.divisibleByThree(4);
         assertEquals(false, result);
     }
