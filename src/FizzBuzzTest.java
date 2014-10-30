1c007209b9c7c383a554a8152a0c07584fbaca8e
Sarah <y.sarahc@gmail.com>
2014-10-30 11:16:14 +0000

    Adds notDivisibleByThree failing  test

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -10,4 +10,11 @@ public class FizzBuzzTest {
         boolean result = game.divisibleByThree(3);
         assertEquals(true, result);
     }
+
+    @Test
+    public void notDivisibleByThree() {
+        FizzBuzz game = new FizzBuzz();
+        boolean result = game.divisibleByThree(4);
+        assertEquals(false, result);
+    }
 }
