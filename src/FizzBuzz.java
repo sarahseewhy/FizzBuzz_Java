ab199a654259ae3dfec81139aee25a15c2121d3b
Sarah <y.sarahc@gmail.com>
2014-10-30 11:18:46 +0000

    Adds logic to make divisibleByThree tests pass

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,7 +2,12 @@
  * Created by sarah on 10/30/14.
  */
 public class FizzBuzz {
+    //Checks if a number is divisible by three
     public boolean divisibleByThree(int number) {
-        return true;
+        boolean result = false;
+        if(number % 3 == 0) {
+            result = true;
+        }
+        return result;
     }
 }
